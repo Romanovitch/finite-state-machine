bf1c3ab8868936b05748e8744904a10a0f0503a9
Romanovitch <rokos.job.ru@gmail.com>
2019-10-14 23:15:56 +0300

    feat: implement task

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,63 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        this.config = config;
+        if (!config) {throw new Error()};
+        this.history = [{step: 0, start: "", end: "normal", undo: false}];
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.currentStep().end;
+    }
+
+    currentStep() {
+        const  currentSteps = this.history.filter(el => el.undo === false);
+        return currentSteps[currentSteps.length - 1];
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if(this.config.states.hasOwnProperty(state)){
+            this.history = this.history.concat(
+                {
+                    step: ++this.currentStep().step,
+                    start: this.currentStep().end,
+                    end: state,
+                    undo: false,
+                });
+            this.history = this.history.filter(el => el.undo !== true);
+        } else {
+            throw new Error();
+        }
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        const nextState = this.config.states[this.currentStep().end].transitions[event];
+        if(nextState){
+            this.changeState(nextState);
+        } else {
+            throw new Error();
+        }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.changeState(this.config.initial);
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +67,54 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        if (!event) {return Object.getOwnPropertyNames(this.config.states)}        
+        const resArr = Object.getOwnPropertyNames(this.config.states)
+            .filter(iState => 
+                Object.getOwnPropertyNames(this.config.states[iState].transitions)
+                .some(iEvent => iEvent === event)
+            );
+            return (resArr.length) ? resArr : [];
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        const thisRecord = this.currentStep();
+        if (thisRecord.start) {
+            if (thisRecord.end !== thisRecord.start) {
+                thisRecord.undo = true;
+                return true;
+            } 
+        } else {
+            return false;
+        }
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        const firstUndoStep = this.history.filter(el => el.undo === true)[0];
+        if (firstUndoStep) {
+            firstUndoStep.undo = false;
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.history = [{step: 0, start: "", end: "normal", undo: false}];
+    }
 }
 
 module.exports = FSM;
